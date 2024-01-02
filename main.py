2aaa027fcaa4675418295b289d229d88c7048792
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-01-02 18:06:00 +0100

    settings + assets beállítás

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import pygame
-
-#állandó értékek
-WIDTH=1280
-HEIGHT=620
-BG_COLOR=(255,255,255)
-FPS=60
+from settings import * #settings.py fájlból behúz mindent
 
 pygame.init() #inicializálja magát a pygame
 screen=pygame.display.set_mode((WIDTH,HEIGHT)) #meghatározza az ablakot
 pygame.display.set_caption('My Firs Platformer') #főcím
 clock=pygame.time.Clock() #időzítő
 
-running=True #futás
+#háttérkép
+bg_surf=pygame.image.load(BG_IMAGE).convert_alpha()
+bg_rect=bg_surf.get_rect(bottomleft=(0,HEIGHT))
+
+running=True #futtatás
 while running:
-    for event in pygame.event.get(): #események
+    for event in pygame.event.get(): #események loop
         if event.type==pygame.QUIT: #kilépés
             running=False
 
     screen.fill(BG_COLOR) #háttérszín
+    screen.blit(bg_surf,bg_rect) #háttérkép
 
     pygame.display.update() #frissítés
     clock.tick(FPS)
