6d652a0327688ebd827cc55d029d2609fe34ed6c
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-01-03 17:16:03 +0100

    __name__== main

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-import pygame
-from settings import * #settings.py fájlból behúz mindent
-from level import Level
+if __name__ == "__main__": #chatgpt szerint ez hasznos, hogy jól fussanak le a fájlok
+    import pygame
+    from settings import * #settings.py fájlból behúz mindent
+    from level import Level
 
-pygame.init() #inicializálja magát a pygame
-screen=pygame.display.set_mode((WIDTH,HEIGHT)) #meghatározza az ablakot
-pygame.display.set_caption('My Firs Platformer') #főcím
-clock=pygame.time. Clock() #időzítő
+    pygame.init() #inicializálja magát a pygame
+    screen=pygame.display.set_mode((WIDTH,HEIGHT)) #meghatározza az ablakot
+    pygame.display.set_caption('My Firs Platformer') #főcím
+    clock=pygame.time. Clock() #időzítő
 
-#háttérkép
-bg_surf=pygame.image.load(BG_IMAGE).convert_alpha()
-bg_rect=bg_surf.get_rect(bottomleft=(0,HEIGHT))
+    #háttérkép
+    bg_surf=pygame.image.load(BG_IMAGE).convert_alpha()
+    bg_rect=bg_surf.get_rect(bottomleft=(0,HEIGHT))
 
-#szint
-level=Level(level_map,screen)#hozz létre egy szint objektumot a levelmap(ami most settingsben van) alapján a screenre
+    #szint
+    level=Level(level_map,screen)#hozz létre egy szint objektumot a levelmap(ami most settingsben van) alapján a screenre
 
-running=True #futtatás
-while running:
-    for event in pygame.event.get(): #események loop
-        if event.type==pygame.QUIT: #kilépés
-            running=False
+    running=True #futtatás
+    while running:
+        for event in pygame.event.get(): #események loop
+            if event.type==pygame.QUIT: #kilépés
+                running=False
 
-    screen.fill(BG_COLOR) #háttérszín
-    screen.blit(bg_surf,bg_rect) #háttérkép
+        screen.fill(BG_COLOR) #háttérszín
+        screen.blit(bg_surf,bg_rect) #háttérkép
 
-    level.run() #szint megrajzolása a képernyőn és az azon történő események futtatása
+        level.run() #szint megrajzolása a képernyőn és az azon történő események futtatása
 
-    pygame.display.update() #frissítés
-    clock.tick(FPS)
+        pygame.display.update() #frissítés
+        clock.tick(FPS)
 
-pygame.quit()
+    pygame.quit()
